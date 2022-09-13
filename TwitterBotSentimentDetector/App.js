fe38a56a120c7fca3f966c279a8070f727389f97
Nick Gutierrez <ng0805@bu.edu>
2022-09-13 08:17:10 -0400

    Basic Text Input Submit and OnChange

---
+++ TwitterBotSentimentDetector/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
+
+const UselessTextInput = () => {
+    const [text, onChangeText] = React.useState(null);
+    const [number, onChangeNumber] = React.useState(null);
+
+    return (
+        <SafeAreaView>
+            <TextInput
+                style={styles.input}
+                onSubmitEditing={(value) => onChangeText(value.nativeEvent.text)}
+                placeholder="Text Submit Placeholder"
+            />
+            <Text>Submit Text: {text} </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeNumber}
+                value={number}
+                placeholder="Numeric Change Placeholder"
+                keyboardType="numeric"
+            />
+            <Text>Changed Num: {number}</Text>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
+
+export default UselessTextInput;
