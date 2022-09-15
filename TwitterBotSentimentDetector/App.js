6a00479877953be84cb93b74e40b2f9bae9795a7
Nick Gutierrez <ng0805@bu.edu>
2022-09-15 12:11:34 -0400

    Google Sign-In Added

--- TwitterBotSentimentDetector/App.js
+++ TwitterBotSentimentDetector/App.js
@@ -1,120 +1,87 @@
 import React, {useEffect} from "react";
-import { SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {SafeAreaView, StyleSheet, TextInput, Text, Button} from "react-native";
 import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
 
-const Stack = createNativeStackNavigator();
-
 const App = () => {
-    const [text, onChangeText] = React.useState(null);
-    const [loggedIn, setloggedIn] = useState(false);
-    const [userInfo, setuserInfo] = useState([]);
 
-    const LoginScreen = ({ navigation }) => {
-        return(
-            <SafeAreaView>
-                <GoogleSigninButton
-                    style={{width: 192, height: 48}}
-                    size={GoogleSigninButton.Size.Wide}
-                    color={GoogleSigninButton.Color.Dark}
-                    onPress={() => googleSignIn(navigation)}
-                />
-                <TextInput
-                    style={styles.input}
-                    onSubmitEditing={(value) => displayText(value.nativeEvent.text, navigation)}
-                    placeholder="Enter Login to go to profile page"
-                />
-                <Text>Submit Text: {text} </Text>
-            </SafeAreaView>
-        )
-    }
-
-    const ProfileScreen = ({ navigation}) => {
-        return (
-            <SafeAreaView>
-                <button onClick={() => googleSignOut(navigation)}>
-                    Sign Out
-                </button>
-                <Text>This is Nick's profile</Text>
-                <TextInput
-                    style={styles.input}
-                    onSubmitEditing={(value) => displayText(value.nativeEvent.text, navigation)}
-                    placeholder="Submit Sign Out to return to login page"
-                />
-                <Text>Profile Text: {text} </Text>
-            </SafeAreaView>
-        )
-    };
-
-    let displayText = (val, navigation) => {
-        if(val ===  "Login".toLowerCase()){
-            navigation.navigate('Profile')
-            onChangeText("");
-        }
-        else if(val === "Sign Out".toLowerCase()){
-            navigation.navigate('Login')
-            onChangeText("");
-        }
-        else {
-            onChangeText(val);
-        }
-    };
-
-    let googleSignIn = async (navigation) => {
-        try {
-            await GoogleSignin.hasPlayServices();
-            const {accessToken, idToken} = await GoogleSignin.signIn();
-            setloggedIn(true);
-            navigation.navigate('Profile');
-        } catch (error) {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                navigation.navigate('Login');
-                alert('Cancel');
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-                alert('Signin in progress');
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                alert('PLAY_SERVICES_NOT_AVAILABLE');
-                navigation.navigate('Login');
-            } else {
-                alert('Error Signing In');
-                navigation.navigate('Login');
-            }
-        }
-    };
+    const [loggedIn, setloggedIn] = React.useState("false");
+    const [userInfo, setuserInfo] = React.useState([]);
+    const [buttonPress, setButtonPress] = React.useState("Unpressed");
 
     useEffect(() => {
         GoogleSignin.configure({
             scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
             webClientId:
-                '418977770929-g9ou7r9eva1u78a3anassxxxxxxx.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+                '1003272958417-uh10sb2pfgjvts2jf9mhhu61n4s5mp4k.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
             offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
         });
     }, []);
 
-    let googleSignOut = async (navigation) => {
+    let _signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const {accessToken, idToken} = await GoogleSignin.signIn();
+            setloggedIn("true");
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+                setloggedIn("canceled");
+                alert('Cancel');
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                alert('Sign in in progress');
+                // operation (f.e. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                alert('PLAY_SERVICES_NOT_AVAILABLE');
+                // play services not available or outdated
+            } else {
+                // some other error happened
+            }
+        }
+    };
+
+    let signOut = async () => {
         try {
             await GoogleSignin.revokeAccess();
             await GoogleSignin.signOut();
-            setloggedIn(false);
+            setloggedIn("false");
             setuserInfo([]);
-            navigation.navigate('Login');
         } catch (error) {
             console.error(error);
         }
     };
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Login"
-                    component={LoginScreen}
-                />
-                <Stack.Screen name="Profile" component={ProfileScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+    let buttonChange = () => {
+        if(buttonPress === "Unpressed"){
+            setButtonPress("Pressed");
+        }
+        else{
+            setButtonPress("Unpressed");
+        }
+    }
+
+    return(
+        <SafeAreaView>
+            <GoogleSigninButton
+                style={{ width: 192, height: 48 }}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={_signIn}/>
+
+            <Text>IsLoggedIn: {loggedIn} </Text>
+
+            <Button
+                title="Push Me"
+                color="#841584"
+                onPress={() => buttonChange()}
+            />
+            <Text>Button: {buttonPress}</Text>
+            <Button
+                title="Sign Out"
+                color="#841584"
+                onPress={() => signOut()}
+            />
+        </SafeAreaView>
+    )
 };
 
 const styles = StyleSheet.create({
